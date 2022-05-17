4131313682828b1d75ab8398bcf28304cf5ee0c8
Omotolani Olurotimi <omotolaniolurotimi@gmail.com>
2022-05-17 15:10:25 +0100

    Added Spatie/Sluggable for generation of Unique Slugs

--- public/js/extra.js
+++ public/js/extra.js
@@ -1,1 +1,25 @@
-console.log("Working")
+$ = jQuery;
+
+const BASE_URL = "http://127.0.0.1:8000/api/";
+
+$.ajaxSetup({
+    headers: {
+        'X-CSRF-TOKEN': $('meta[name="_token"]').attr('content'),
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+    }
+});
+
+if($("#movies")){
+    $.ajax({
+        type: "GET",
+        url: BASE_URL+"movies",
+        data: "",
+        success: function(data){
+            console.log(data);
+        },
+        error: function(data){
+            console.log(data.responseText);
+        }
+    })
+}
