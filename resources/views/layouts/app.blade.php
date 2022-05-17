84bd346de403ddc0d0a14f84969642b80c702fb9
Omotolani Olurotimi <omotolaniolurotimi@gmail.com>
2022-05-17 14:03:02 +0100

    Addition of jQuery and extra.js

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -79,5 +79,7 @@
             @yield('content')
         </main>
     </div>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+    <script src="{{asset('/js/extra.js')}}"></script>
 </body>
 </html>
--- resources/views/movies.blade.php
+++ resources/views/movies.blade.php
@@ -1,5 +1,9 @@
 @extends('layouts.app')
 
 @section('content')
-    
+<div class="container">
+    <div class="row justify-content-center" id="all_movies">
+
+    </div>
+</div>
 @endsection
